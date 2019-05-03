5ccd0052d95dff5025cf826d00af819e9c9c437f
AlexGioffre <alexgioffre1191@gmail.com>
2019-05-03 16:44:51 +0200

    db connection

--- db/connection.js
+++ db/connection.js
@@ -3,12 +3,12 @@ const knexConnect = require('knex');
 const database = knexConnect({
     client: 'pg',
     connection: {
-        // connectionString: process.env.DATABASE_URL,
-        // ssl: true,
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'Bakuman91',
-        database: 'Razeft-db'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+        // host: '127.0.0.1',
+        // user: 'postgres',
+        // password: 'Bakuman91',
+        // database: 'Razeft-db'
     }
 });
 
