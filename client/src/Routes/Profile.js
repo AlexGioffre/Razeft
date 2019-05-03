3ff86d83af62d5a165fd98febf7e26ee03412032
AlexGioffre <alexgioffre1191@gmail.com>
2019-05-03 12:32:57 +0200

    Fi 9

--- client/src/Routes/Profile.js
+++ client/src/Routes/Profile.js
@@ -19,13 +19,13 @@ class Profile extends Component {
     componentDidMount(){
         if(this.props.auth.user){
             console.log(this.props.auth.user);
+            this.setState({i: 0});
             this.loopMovie();
         }
     }
 
     loopMovie = () => {
         if(this.props.auth.user){
-            this.setState({i: 0})
             if(this.props.auth.user.movies === null || this.props.auth.user.movies === undefined  ){
                 this.setState({movies: [], load: false, y: 0});
                 this.loopSeries();
